48730aa230e9b514c7243589e1d9e560bfd9de65
Jenrg23 <116044217+Jenrg23@users.noreply.github.com>
2026-04-15 00:43:37 -0600

    Refactor AspiranteDTO to separate name fields
    Split NombreCompleto into Nombre and Apellido properties.

--- SistemaMatricula/Models/AspiranteDTO.cs
+++ SistemaMatricula/Models/AspiranteDTO.cs
@@ -1,11 +1,19 @@
-﻿namespace SistemaMatricula.Models
+namespace SistemaMatricula.Models
 {
     public class AspiranteDTO
     {
         public int IdAspirante { get; set; }
-        public string NombreCompleto { get; set; }
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; } //mias
+
+        public string NombreCompleto { get; set; } // tuya
+
         public string Email { get; set; }
+        public string Telefono { get; set; }
+
         public string CarreraInteres { get; set; }
+
         public bool EstaMatriculado { get; set; }
     }
 
